e89afb411d7f1697ac303b75ebb73b91606c1050
BLA4CKK <lopesmachadopedrohenrique@gmail.com>
2026-05-07 15:48:13 -0300

    Alteração na pagina home

--- src/pages/Principal/Home.jsx
+++ src/pages/Principal/Home.jsx
@@ -1,10 +1,14 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
 import { LiaClipboardListSolid } from "react-icons/lia";
 export default function Home() {
+
+    const navigate = useNavigate()
+
   return (
     <>
       <Cabecalho />
-      <div className="flex justify-center h-full ">
+      <div className="flex justify-center h-full" onClick={()=> navigate("/Checklist")}>
         <div className="flex flex-col bg-[#ae0909] w-80 h-70 justify-center items-center rounded-4xl m-28">
           <LiaClipboardListSolid color="white" size="200" />
           <div className="flex items-center bg-[#850808]  h-9 w-50">
@@ -12,7 +16,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex justify-end bg-[#ae0909] w-full h-21"></div>
+      <footer className="flex justify-end bg-[#ae0909] w-full h-21"></footer>
     </>
   );
 }
